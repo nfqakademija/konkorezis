dcee76f2f60696c6197a9ba3f55357f2b5ef2bcc
JurijBeliavskij <jurij.beliavskij@gmail.com>
2015-12-01 15:10:51 +0200

    Added and basicly configured FosUserBundle. Changed other files according to needs.

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -8,30 +8,6 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class UserController extends Controller
 {
-    // TODO: to be implemented after reading additional information about FOSUserBundle
-    /**
-     * @Route(
-     *     "/user/login/",
-     *     name="user_login"
-     * )
-     */
-    public function loginAction()
-    {
-        return new RedirectResponse($this->generateUrl('orders_open'));
-    }
-
-    // TODO: to be implemented after reading additional information about FOSUserBundle
-    /**
-     * @Route(
-     *     "/user/logout",
-     *     name="user_logout"
-     * )
-     */
-    public function logoutAction()
-    {
-        return new RedirectResponse($this->generateUrl('homepage'));
-    }
-
     /**
      * @Route(
      *     "/user/history/{per_page}/{page_number}",
@@ -70,38 +46,6 @@ class UserController extends Controller
             'per_page'      => $per_page,
             'page_number'   => $page_number
         ));
-    }
-
-    // TODO: to be implemented after reading additional information about FOSUserBundle
-    /**
-     * @Route(
-     *     "/user/forgot-password",
-     *     name="user_forgot_password"
-     * )
-     */
-    public function forgotPasswordAction()
-    {
-        // TODO: render forgot password view
-        return $this->render('default/index.html.twig');
-    }
-
-    // TODO: to be implemented after reading additional information about FOSUserBundle
-    /**
-     * @Route(
-     *     "/user/change-password",
-     *     name="user_change_password"
-     * )
-     */
-    public function changePasswordAction()
-    {
-        // Create flash message for successful password change
-        $this->addFlash(
-            'notice',
-            'Your password has been successfully changed. Check your email for more information!'
-        );
-
-        // Redirect to homepage screen
-        return new RedirectResponse($this->generateUrl('homepage'));
     }
 }
 
--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Entity;
 
+use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -11,7 +12,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table()
  * @ORM\Entity
  */
-class User
+class User extends BaseUser
 {
     /**
      * @var integer
@@ -20,22 +21,7 @@ class User
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      */
-    private $id;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="email", type="string", length=255)
-     */
-    private $email;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="password", type="string", length=255)
-     */
-    private $password;
-
+    protected $id;
 
     /**
      * Get id
@@ -46,55 +32,6 @@ class User
     {
         return $this->id;
     }
-
-    /**
-     * Set email
-     *
-     * @param string $email
-     *
-     * @return User
-     */
-    public function setEmail($email)
-    {
-        $this->email = $email;
-
-        return $this;
-    }
-
-    /**
-     * Get email
-     *
-     * @return string
-     */
-    public function getEmail()
-    {
-        return $this->email;
-    }
-
-    /**
-     * Set password
-     *
-     * @param string $password
-     *
-     * @return User
-     */
-    public function setPassword($password)
-    {
-        $this->password = $password;
-
-        return $this;
-    }
-
-    /**
-     * Get password
-     *
-     * @return string
-     */
-    public function getPassword()
-    {
-        return $this->password;
-    }
-
 
     /**
      * @ORM\OneToMany(targetEntity="Orders", mappedBy="user")
@@ -108,6 +45,7 @@ class User
 
     public function __construct()
     {
+        parent::__construct();
         $this->orders = new ArrayCollection();
         $this->userProducts = new ArrayCollection();
     }
